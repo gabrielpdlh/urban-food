34dbcb0750ee60f7973daae0cb6c67a858c7495a
Gabriel Padilha <gabriel.padilha1204@gmail.com>
2024-05-01 15:28:44 -0300

    feat: add product list and product component

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,9 @@ import Image from "next/image";
 import CategoryList from "./_components/category-list";
 import Header from "./_components/header";
 import Search from "./_components/search";
+import ProductList from "./_components/product-list";
+import { Button } from "./_components/ui/button";
+import { ChevronRightIcon } from "lucide-react";
 
 const Home = () => {
   return (
@@ -25,6 +28,19 @@ const Home = () => {
           sizes="100vw"
           quality={100}
         />
+      </div>
+
+      <div className="pt-6 space-y-4">
+        <div className="flex justify-between items-center px-5">
+          <h2 className="font-semibold">Pedidos Recomendados</h2>
+          <Button
+            variant="ghost"
+            className="h-fit text-primary p-0 hover:bg-transparent"
+          >
+            Ver todos <ChevronRightIcon size={16} />
+          </Button>
+        </div>
+        <ProductList />
       </div>
     </>
   );
