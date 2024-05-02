ee3750fe86f10cbd9650172dd14ebc8c1ffe9a40
Gabriel Padilha <gabriel.padilha1204@gmail.com>
2024-05-02 18:04:12 -0300

    feat: add link at product-item

--- app/_components/product-tem.tsx
+++ app/_components/product-tem.tsx
@@ -2,6 +2,7 @@ import { Prisma } from "@prisma/client";
 import Image from "next/image";
 import { calculateProductTotalPrice, formatCurrency } from "../_helpers/price";
 import { ArrowDownIcon } from "lucide-react";
+import Link from "next/link";
 
 interface ProductItemProps {
   product: Prisma.ProductGetPayload<{
@@ -17,46 +18,48 @@ interface ProductItemProps {
 
 const ProductItem = ({ product }: ProductItemProps) => {
   return (
-    <div className="space-y-2 min-w-[150px] w-[150px]">
-      <div className="h-[150px] w-full relative">
-        <Image
-          src={product.imageUrl}
-          alt={product.name}
-          fill
-          className="object-cover rounded-lg shadow-md"
-        />
+    <Link href={`/products/${product.id}`} className="min-w-[150px] w-[150px]">
+      <div className="space-y-2 w-full">
+        <div className="h-[150px] w-full relative">
+          <Image
+            src={product.imageUrl}
+            alt={product.name}
+            fill
+            className="object-cover rounded-lg shadow-md"
+          />
 
-        {product.discountPercentage && (
-          <div
-            className="absolute gap-[2px] top-2 left-2 bg-primary px-2 py-[2px] rounded-full
+          {product.discountPercentage && (
+            <div
+              className="absolute gap-[2px] top-2 left-2 bg-primary px-2 py-[2px] rounded-full
                  text-white flex items-center"
-          >
-            <ArrowDownIcon size={12} />
-            <span className="text-xs font-semibold">
-              {product.discountPercentage}%
-            </span>
-          </div>
-        )}
-      </div>
-
-      <div>
-        <h2 className="text-sm truncate">{product.name}</h2>
-        <div className="flex items-center gap-1">
-          <h3 className="font-semibold">
-            {formatCurrency(calculateProductTotalPrice(product))}
-          </h3>
-          {product.discountPercentage > 0 && (
-            <span className="text-xs line-through text-muted-foreground">
-              {formatCurrency(Number(product.price))}
-            </span>
+            >
+              <ArrowDownIcon size={12} />
+              <span className="text-xs font-semibold">
+                {product.discountPercentage}%
+              </span>
+            </div>
           )}
         </div>
 
-        <span className="block text-muted-foreground text-xs">
-          {product.restaurant.name}
-        </span>
+        <div>
+          <h2 className="text-sm truncate">{product.name}</h2>
+          <div className="flex items-center gap-1">
+            <h3 className="font-semibold">
+              {formatCurrency(calculateProductTotalPrice(product))}
+            </h3>
+            {product.discountPercentage > 0 && (
+              <span className="text-xs line-through text-muted-foreground">
+                {formatCurrency(Number(product.price))}
+              </span>
+            )}
+          </div>
+
+          <span className="block text-muted-foreground text-xs">
+            {product.restaurant.name}
+          </span>
+        </div>
       </div>
-    </div>
+    </Link>
   );
 };
 
